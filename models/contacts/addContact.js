128701cd5c7abb14b74110daede5a1f398c963e5
Yurii Yurents <yura19950117@gmail.com>
2021-11-06 12:51:44 +0200

    fix addContact id

--- models/contacts/addContact.js
+++ models/contacts/addContact.js
@@ -3,10 +3,11 @@ const crypto = require('crypto')
 const getAll = require('./getAll')
 const updateContacts = require('./updateContacts')
 
-const id = crypto.randomBytes(16).toString('hex')
+const getId = () => crypto.randomBytes(16).toString('hex')
 
 const addContact = async (data) => {
   const contacts = await getAll()
+  const id = getId()
   const newContact = { id, ...data }
   contacts.push(newContact)
   await updateContacts(contacts)
