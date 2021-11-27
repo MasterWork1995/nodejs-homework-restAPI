6d146c57123df5ab210e3a77a5f7607a4a6a49e0
Yurii Yurents <yura19950117@gmail.com>
2021-11-27 15:27:32 +0200

    add updateAvatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,21 +1,24 @@
-// const bcrypt = require('bcryptjs')
-
 const { User } = require('../../models')
 const { sendConflict, sendSuccessToRes } = require('../../helpers')
+const gravatar = require('gravatar')
+const fs = require('fs/promises')
+const path = require('path')
+const avatarsDir = path.join(__dirname, '../../public/avatars')
 
 const register = async (req, res) => {
   const { email, password } = req.body
+  const avatarURL = gravatar.url(email)
   const user = await User.findOne({ email })
   if (user) {
     sendConflict(email)
   }
-  const newUser = new User({ email })
+  const newUser = new User({ email, avatarURL })
   newUser.setPassword(password)
+  const avatarFolder = path.join(avatarsDir, String(newUser._id))
+  await fs.mkdir(avatarFolder)
   await newUser.save()
   const { subscription } = newUser
 
-  // const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-  // await User.create({ email, password: hashPassword })
   sendSuccessToRes(res, { email, subscription }, 201)
 }
 
